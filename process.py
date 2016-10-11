99ae401000185d92c2470eeac6af1d0931ae79ce
epesco <epesco@wpi.edu>
2016-10-11 11:40:02 -0400

    divide data and cross validate

--- process.py
+++ process.py
@@ -3,6 +3,7 @@ import json
 from nltk.classify import NaiveBayesClassifier
 import random
 import math
+import copy
 
 def format_observed_weather():
     previous_weather = None
@@ -56,14 +57,24 @@ def compute_features(tweets_by_weather):
     final_data = label_data(clear_featuresets, 'Clear') + label_data(cloudy_featuresets, 'Clouds')
     return final_data
 
-def divide_data(final_data):
+def divide_data(final_data, folds):
+    data_chunks = []
     data_amount = len(final_data)
-    train_amount = int(math.floor(0.8*data_amount))
-    print train_amount
     random.shuffle(final_data)
-    train_data = final_data[:train_amount]
-    test_data = final_data[train_amount+1:]
-    return train_data, test_data
+    step_size = int(math.ceil(data_amount/folds))
+    remainder = data_amount % folds
+    for i in range(0, data_amount, step_size):
+        train_data = copy.deepcopy(final_data) #justpythonthings
+        if i + step_size == data_amount - remainder:
+            test_data = train_data[i:i+step_size+remainder]
+            train_data[i:i+step_size+remainder] = []
+            data_chunks.append(test_data)
+            break
+        else:
+            test_data = train_data[i:i+step_size]
+            train_data[i:i+step_size] = []
+        data_chunks.append(test_data)
+    return data_chunks
 
 # Get observed weather in formatted time ranges
 observed_weather = format_observed_weather()
@@ -71,9 +82,28 @@ observed_weather = format_observed_weather()
 tweets_by_weather = find_tweets_weather(observed_weather)
 # Compute features (words) and their labels
 final_data = compute_features(tweets_by_weather)
-train_data, test_data = divide_data(final_data)
+data_chunks = divide_data(final_data, 5)
 
-classifier = NaiveBayesClassifier.train(final_data)
-classifier.show_most_informative_features()
-print classifier.classify(features('Loving my pumpkin spice latte in dis coat'))
+def cross_validate(data_chunks):
+    train_data = []
+    averages = []
+    for i in range(0,len(data_chunks)):
+        correct, total = 0, 0
+        train_chunks = data_chunks
+        test_chunk = train_chunks[i]
+        train_chunks[i] = []
+        for chunk in train_chunks:
+            for tweet in chunk:
+                train_data.append(tweet)
+        classifier = NaiveBayesClassifier.train(train_data)
+        for tweet in test_chunk:
+            total += 1
+            if tweet[1] == classifier.classify(tweet[0]):
+               correct += 1
+        accuracy = float(correct) / float(total)
+        averages.append(accuracy)
+        print "Fold: " + str(i+1) + ", Accuracy: " + str(accuracy)
+    return sum(averages)/len(averages)
+print "AVERAGE ACCURACY: " + str(cross_validate(data_chunks))
+#classifier.show_most_informative_features()
 
