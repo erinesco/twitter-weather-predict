06a29d36e6eacd32fff2bb0be9e69d73b1383ca0
Michael McConnell <msmcconnell@wpi.edu>
2016-10-10 22:47:54 -0400

    sorta working svm

--- process.py
+++ process.py
@@ -1,7 +1,13 @@
 # Combine twitter and weather data to produce tweets labeled by weather
 import json
-from nltk.classify import NaiveBayesClassifier
+import sys
+import codecs
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.svm import LinearSVC
+from nltk.classify.scikitlearn import SklearnClassifier
 from functools import partial
+
 def format_observed_weather():
     previous_weather = None
     observed_weather = {}
@@ -54,13 +60,72 @@ def compute_features(tweets_by_weather):
     final_data = new_clear + new_cloudy
     return final_data
 
+def compute_features_decision_tree(tweets_by_weather):
+    clear_featuresets = list(map(features, tweets_by_weather['Clear']))
+    cloudy_featuresets = list(map(features, tweets_by_weather['Clouds']))
+    new_clear, new_cloudy = [], []
+
+
+    for element in clear_featuresets:
+        new_clear.append((element, 'Clear'))
+    for element in cloudy_featuresets:
+        new_cloudy.append((element, 'Cloudy'))
+    final_data = new_clear + new_cloudy
+    return final_data
+
+def decision_tree_train():
+	# Get observed weather in formatted time ranges
+	observed_weather = format_observed_weather()
+	# Assign tweets to their respective time range and weather
+	tweets_by_weather = find_tweets_weather(observed_weather)
+	# Compute features (words) and their labels
+	final_data = compute_features_decision_tree(tweets_by_weather)
+	#print final_data
+
+	#word count
+	#critical words
+	#Capitol counts
+
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 # Get observed weather in formatted time ranges
 observed_weather = format_observed_weather()
 # Assign tweets to their respective time range and weather
 tweets_by_weather = find_tweets_weather(observed_weather)
+
 # Compute features (words) and their labels
 final_data = compute_features(tweets_by_weather)
+final_data = compute_features_decision_tree(tweets_by_weather)
+#print final_data
 
-classifier = NaiveBayesClassifier.train(final_data)
-classifier.show_most_informative_features()
+classif = SklearnClassifier(LinearSVC(multi_class='crammer_singer'))
+classif.train(final_data)
+count_correct = 0
+count_total = 0
+count_clouds = 0
+for classification, class_tweets in tweets_by_weather.iteritems():
+	
+	for tweet in class_tweets:
+		result = classif.classify_many(features(tweet))
+		if (result[0] == classification):
+			count_correct += 1
+		if classification.strip().lower() == u'cloudy':
+			count_clouds += 1
+		count_total +=1
+print 'Correct ' + str(count_correct)
+print 'Total ' + str(count_total)
+print 'Percentage Correct ' + str(float(count_correct) / float(count_total))
+print 'clouds ' + str(count_clouds)
+#result = classif.classify_many(features('yo yo yo mathew'))
+print result
+print classif.labels()
+#classifier = nltk.classify.NaiveBayesClassifier.train(final_data)
+#classifier.show_most_informative_features()
+#dt_classifier = nltk.classify.decisiontree.DecisionTreeClassifier.train(final_data)
+#print nltk.sentiment.vader.SentiText('I am angry')._words_and_emoticons()
+st = SentimentIntensityAnalyzer()
+print st.polarity_scores('I am Happy and Angry') #This works
+#dt_classifier.train(final_data)
+
 
