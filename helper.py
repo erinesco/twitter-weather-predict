9de26adada5616a51743b065ede73daac3c3c023
epesco <epesco@wpi.edu>
2016-10-11 12:53:44 -0400

    helper file

--- helper.py
+++ helper.py
@@ -1,20 +1,8 @@
-# Combine twitter and weather data to produce tweets labeled by weather
 import json
-<<<<<<< HEAD
-import sys
-import codecs
-
-#from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from sklearn.svm import LinearSVC
-from nltk.classify.scikitlearn import SklearnClassifier
-from functools import partial
-
-=======
->>>>>>> fe80aae219817c503f57185739a372660c1339c9
-from nltk.classify import NaiveBayesClassifier
 import random
 import math
 import copy
+from nltk.classify import NaiveBayesClassifier
 
 def format_observed_weather():
     previous_weather = None
@@ -65,71 +53,9 @@ def label_data(featureset, label):
 def compute_features(tweets_by_weather):
     clear_featuresets = list(map(features, tweets_by_weather['Clear']))
     cloudy_featuresets = list(map(features, tweets_by_weather['Clouds']))
-    final_data = label_data(clear_featuresets, 'Clear') + label_data(cloudy_featuresets, 'Clouds')
+    rainy_featuresets = list(map(features, tweets_by_weather['Rain']))
+    final_data = label_data(clear_featuresets, 'Clear') + label_data(cloudy_featuresets, 'Clouds') + label_data(rainy_featuresets, 'Rain')
     return final_data
-
-def compute_features_decision_tree(tweets_by_weather):
-    clear_featuresets = list(map(features, tweets_by_weather['Clear']))
-    cloudy_featuresets = list(map(features, tweets_by_weather['Clouds']))
-    rain_featuresets = list(map(features, tweets_by_weather['Rain']))
-    new_clear, new_cloudy, new_rain = [], [], []
-
-
-    for element in clear_featuresets:
-        new_clear.append((element, 'Clear'))
-    for element in cloudy_featuresets:
-        new_cloudy.append((element, 'Clouds'))
-    for element in rain_featuresets:
-        new_rain.append((element, 'Rain'))
-    final_data = new_clear + new_cloudy + new_rain
-    return final_data
-
-def svm(): #Ignore this function for right now. It works but I need to test it with cross validation still
-	sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-	sys.stderr = codecs.getwriter('utf8')(sys.stderr)
-	# Get observed weather in formatted time ranges
-	observed_weather = format_observed_weather()
-	# Assign tweets to their respective time range and weather
-	tweets_by_weather = find_tweets_weather(observed_weather)
-
-	# Compute features (words) and their labels
-	final_data = compute_features(tweets_by_weather)
-	final_data = compute_features_decision_tree(tweets_by_weather)
-	#print final_data
-
-	classif = SklearnClassifier(LinearSVC(multi_class='crammer_singer'))
-	classif.train(final_data)
-	count_correct = 0
-	count_total = 0
-	count_clouds = 0
-	count_rain = 0
-	for classification, class_tweets in tweets_by_weather.iteritems():
-		print '-----' + classification + '--------------'
-		for tweet in class_tweets:
-			result = classif.classify_many(features(tweet))
-			#print result
-			if result[0] == classification:
-				count_correct += 1
-			if classification == 'Clouds':
-				count_clouds += 1
-			if classification == 'Rain':
-				count_rain += 1
-			count_total +=1
-	print 'Correct ' + str(count_correct)
-	print 'Total ' + str(count_total)
-	print 'Percentage Correct ' + str(float(count_correct) / float(count_total))
-	print 'clouds ' + str(count_clouds)
-	print 'rain ' + str(count_rain)
-	#result = classif.classify_many(features('yo yo yo mathew'))
-	print result
-	print classif.labels()
-	#classifier = nltk.classify.NaiveBayesClassifier.train(final_data)
-	#classifier.show_most_informative_features()
-	#dt_classifier = nltk.classify.decisiontree.DecisionTreeClassifier.train(final_data)
-	#print nltk.sentiment.vader.SentiText('I am angry')._words_and_emoticons()
-	#st = SentimentIntensityAnalyzer()
-	#print st.polarity_scores('I am Happy and Angry') #This works
-	#dt_classifier.train(final_data)
 
 def divide_data(final_data, folds):
     data_chunks = []
@@ -150,14 +76,6 @@ def divide_data(final_data, folds):
         data_chunks.append(test_data)
     return data_chunks
 
-# Get observed weather in formatted time ranges
-observed_weather = format_observed_weather()
-# Assign tweets to their respective time range and weather
-tweets_by_weather = find_tweets_weather(observed_weather)
-# Compute features (words) and their labels
-final_data = compute_features(tweets_by_weather)
-data_chunks = divide_data(final_data, 2)
-
 def cross_validate(data_chunks):
     train_data = []
     averages = []
@@ -177,7 +95,5 @@ def cross_validate(data_chunks):
         accuracy = float(correct) / float(total)
         averages.append(accuracy)
         print "Fold: " + str(i+1) + ", Accuracy: " + str(accuracy)
+    classifier.show_most_informative_features()
     return sum(averages)/len(averages)
-print "AVERAGE ACCURACY: " + str(cross_validate(data_chunks))
-#classifier.show_most_informative_features()
-
